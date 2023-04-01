f5b2907b7130c5615be31020e889cded6776ce42
Aditya <adityaagr.201ec103@gmail.com>
2023-04-01 18:07:15 +0530

    refactored index

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 require('dotenv').config();
-const url = `mongodb+srv://prasanna-8446:${process.env.MONGODB_PASSWORD}@parkviz.prxjsun.mongodb.net/?retryWrites=true&w=majority`; // Replace 'mydatabase' with your database name
 const express = require('express');
 const body_parser=require('body-parser')
 const connectionController = require('./controllers/connection_controller')
-const app = express();
-
-const db = connectionController.connectToDb(url)
-// parse application/x-www-form-urlencoded
-app.use(body_parser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(body_parser.json())
-
 const userRoutes = require('./routes/user');
 const parkingRoutes = require('./routes/parking_slot');
-const { connectToDb } = require('./controllers/connection_controller');
 
+const url = `mongodb+srv://prasanna-8446:${process.env.MONGODB_PASSWORD}@parkviz.prxjsun.mongodb.net/?retryWrites=true&w=majority`; // Replace 'mydatabase' with your database name
+const db = connectionController.connectToDb(url)
+
+//body parsing middlewares
+app.use(body_parser.urlencoded({ extended: false }))
+app.use(body_parser.json())
+
+
+const app = express();
 app.use('/users', userRoutes);
 app.use('/parking_slots', parkingRoutes);
 
