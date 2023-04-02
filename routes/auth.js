1f42d8b3cc2b08b580c58cfe8f6bba946a6fb0dc
Aditya <adityaagr.201ec103@gmail.com>
2023-04-02 13:26:11 +0530

    add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post('/signup',authController.registerUser);
 router.post('/login',authController.loginUser);
-router.get('/logout',authController.logoutUser)
+router.get('/logout',authController.logoutUser  )
 
 module.exports = router;
