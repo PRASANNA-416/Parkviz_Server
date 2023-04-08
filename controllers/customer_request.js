4d82696b0d878ecf0d3377b6440c91d14f0702d9
Prasanna <prasannapm416@gmail.com>
2023-04-08 19:16:09 +0530

    ticket creation when parking slot emtpy

--- controllers/customer_request.js
+++ controllers/customer_request.js
@@ -1,18 +1,18 @@
 const CustomerRequest = require("../models/customer_request");
 
 exports.createCustomerRequest = (req, res) => {
-
     const customerRequestData = {
         ...req.body,
         userId: req.session.user._id// Add the user id to the customerRequestData object
       };
-  const customer = new CustomerRequest(customerRequestData);
+  const customerRequest = new CustomerRequest(customerRequestData);
   
-  customer.save()
+  customerRequest.save()
     .then(() => {
       res.send('Customer Request created successfully');
     })
     .catch((err) => {
+      console.log(err)
       res.status(500).send(err.message);
     });
   }
@@ -23,6 +23,11 @@ exports.updateCustomerRequests = async (req,res) => {
   const updatesServices = await CustomerRequest.findByIdAndUpdate(req.params.customerId,{services,note})
 
   res.status(200).send("Status updated successfully")
+
+
+}
+
+exports.getCustomerRequests = async (req,res) => {
 
 
 }
--- models/customer_request.js
+++ models/customer_request.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const ParkingSlot = require("./parking_slot");
+const Ticket = require("./ticket");
 const customerRequestSchema = new mongoose.Schema({
     userId:{
         type: mongoose.Schema.Types.ObjectId,
@@ -8,11 +10,35 @@ const customerRequestSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    services: [mongoose.Schema.Types.ObjectId],
+    services: [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Service'
+        }
+    ],
     note: {
         type: String
+    },
+    status: {
+        type : Boolean,
+        default : false
     }
 });
+
+customerRequestSchema.pre('save', async function(next){
+    const parkingSlot = await ParkingSlot.findOne({status: false});
+    if (parkingSlot && this.isNew)
+    {
+        await Ticket.create({
+            userId : this.userId,
+            carNo : this.carNumber,
+            slotId : parkingSlot._id
+        })
+        this.status = true;
+    }
+    next();
+
+})
 
 const CustomerRequest = mongoose.model('CustomerRequest', customerRequestSchema);
 module.exports = CustomerRequest
